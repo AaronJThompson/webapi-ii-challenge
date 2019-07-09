a13969d1ba65190b20be8e8a8200368e8975b198
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-09 13:23:28 +0100

    Add POST request

--- posts/routes.js
+++ posts/routes.js
@@ -13,4 +13,23 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    if(!req.body.title || !req.body.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+        return;
+    }
+    db.insert(req.body)
+    .then(data => {
+        db.findById(data.id)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(error => {
+            throw error;
+        })
+    })
+    .catch(error => {
+        res.status(500).json({ error: "There was an error while saving the post to the database." })
+    })
+})
 module.exports = router;
