256abddd5d2b636f49b7a368bb320ffb60007dcf
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-09 13:04:38 +0100

    Move routes to file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const db = require('./data/db');
+const postRoutes = require('./posts/routes');
 
 const server = express();
 
 server.use(express.json());
-
-
+server.use('/api/posts', )
 server.listen(3000, () => {
     console.log('\n*** Server Running on port 3000 ***\n');
 })
